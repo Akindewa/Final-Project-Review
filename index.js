8c04f20d34cee994faff32343f8f2f9c0a20a711
Akindewa <raodhotakindewa@gmail.com>
2024-03-20 22:39:05 +0100

    count-down

--- index.js
+++ index.js
@@ -1,35 +1,27 @@
-// const date = new Date()
-// const day = date.getDate()
-// const hours = date.getHours()
-// const minutes = date.getMinutes()
-// const seconds = date.getSeconds()
-
-// const daysElem = document.querySelector('.number-day');
-// const hoursElem = document.querySelector('.number-hours');
-// const minutesElem = document.querySelector('.number-minutes');
-// const secondsElem = document.querySelector('.number-seconds');
-
-// daysElem.innerHTML = day 
-// hoursElem.innerHTML = hours
-// minutesElem.innerHTML = minutes
-// secondsElem.innerHTML = seconds
+function number(number) {
+  return number < 10 ? `0${number}` : number;
+}
 
 setInterval(function(){
 
-    const date = new Date()
-    const day = date.getDate()
-    const hours = date.getHours()
-    const minutes = date.getMinutes()
-    const seconds = date.getSeconds()
-    
-    const dayElem = document.querySelector('.number-day div')
-    const hoursElem = document.querySelector('.number-hours div')
-    const minutesElem = document.querySelector('.number-minutes div')
-    const secondsElem = document.querySelector('.number-seconds div')
+ 
   
-    dayElem.innerText = day
-    hoursElem.innerText = hours
-    minutesElem.innerText = minutes
-    secondsElem.innerText = seconds
+  // Set the target date for the countdown
+  const targetDate = new Date('2024-03-25T23:59:59');
+  const currentDate = new Date();
+  const remainingTime = targetDate - currentDate;
+  // Calculate the remaining days, hours, minutes, and seconds
+  const days = Math.floor(remainingTime / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((remainingTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((remainingTime % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((remainingTime % (1000 * 60)) / 1000);
+  
+  // Update the countdown elements with the remaining time
+  document.querySelector('.number-day div').innerHTML = number(days);
+  document.querySelector('.number-hours div').innerHTML = number(hours);
+  document.querySelector('.number-minutes div').innerHTML = number(minutes);
+  document.querySelector('.number-seconds div').innerHTML = number(seconds);
+  
   
   },1000)
+
---
+++ readme.md
@@ -0,0 +1,8 @@
+# A launch countdown timer project
+
+## A Fully Functioning Project written in HTML,CSS and JAVASCRIPT showing how to manipulated function to achieve a count-down format
+
+This project is an example of that was built using the following 
+
+* Dom manipulation 
+* 
